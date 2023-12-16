142f37cabba4808a810d49706d656e29c542a3f5
GalileoCap <galileocapp@gmail.com>
2023-12-16 10:26:53 -0300

    2023D1 OK: Nicer part one

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -1,24 +1,23 @@
-use std::{env, fs};
+use std::{env, fs, iter::Iterator};
+
+fn match_first_digit<'a, I>(line : I) -> u32
+    where I : Iterator<Item = char>,
+    {
+    for c in line {
+        if let Some(x) = c.to_digit(10) {
+            return x;
+        }
+    }
+    panic!();
+}
 
 fn main() {
     let args : Vec<String> = env::args().collect(); 
     let fpath = &args[1];
 
-    let mut res = 0;
-    for line in fs::read_to_string(fpath).unwrap().lines() {
-        for c in line.chars() {
-            if let Some(x) = c.to_digit(10) {
-                res += x * 10;
-                break;
-            }
-        }
-
-        for c in line.chars().rev() {
-            if let Some(x) = c.to_digit(10) {
-                res += x;
-                break;
-            }
-        }
-    }
+    let res = fs::read_to_string(fpath).unwrap().lines()
+        .fold(0, |res, line|
+            res + match_first_digit(line.chars()) * 10 + match_first_digit(line.chars().rev())
+        );
     dbg!(res);
 }
