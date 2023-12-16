be17f6a4d8120ca7f4e8c156bcd341384cbea5be
GalileoCap <galileocapp@gmail.com>
2023-12-16 12:24:28 -0300

    2023D2 OK: Part One

--- 2023/day2/src/main.rs
+++ 2023/day2/src/main.rs
@@ -1,7 +1,39 @@
+use regex::Regex;
+use lazy_static::lazy_static;
+
 type RES = u32;
 
+fn is_subset_possible(subset : &String) -> bool {
+    if subset.starts_with("Game") { // Hack for the first part
+        return true
+    }
+
+    lazy_static!{
+        static ref RED : Regex = Regex::new(r"(\d+) red").unwrap();
+        static ref GREEN : Regex = Regex::new(r"(\d+) green").unwrap();
+        static ref BLUE : Regex = Regex::new(r"(\d+) blue").unwrap();
+    }
+
+    RED.captures_iter(&subset).fold(0, |ret, cap| ret + cap[1].parse::<u32>().unwrap()) <= 12 &&
+    GREEN.captures_iter(&subset).fold(0, |ret, cap| ret + cap[1].parse::<u32>().unwrap()) <= 13 &&
+    BLUE.captures_iter(&subset).fold(0, |ret, cap| ret + cap[1].parse::<u32>().unwrap()) <= 14
+}
+
+fn is_game_possible(game : &String) -> bool {
+    game[game.find(":").unwrap()..].split(";").all(|subset| is_subset_possible(&subset.to_string()))
+}
+
 fn solve_part_one(file : &String) -> RES {
-    todo!()
+    lazy_static!{
+        static ref ID : Regex = Regex::new(r"Game (\d+)").unwrap();
+    }
+
+    file.lines().fold(0,
+        |res, line|
+            res + (if is_game_possible(&line.to_string()) {
+                ID.captures(&line.to_string()).unwrap()[1].parse().unwrap()
+            } else { 0 })
+    )
 }
 
 fn solve_part_two(file : &String) -> RES {
@@ -24,7 +56,7 @@ fn main() {
 
     for (i, (file, expected)) in
         [file_1, file, file_2, file].iter()
-            .zip([8, 0, 0, 0])
+            .zip([8, 2164, 0, 0])
             .enumerate()
         {
         use std::time::Instant;
