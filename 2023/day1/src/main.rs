1ff759eb21f7babec5208b950f404639a9f0d00c
GalileoCap <galileocapp@gmail.com>
2023-12-16 10:15:12 -0300

    2023D1 OK: Naive part one

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -1,3 +1,24 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args : Vec<String> = env::args().collect(); 
+    let fpath = &args[1];
+
+    let mut res = 0;
+    for line in fs::read_to_string(fpath).unwrap().lines() {
+        for c in line.chars() {
+            if let Some(x) = c.to_digit(10) {
+                res += x * 10;
+                break;
+            }
+        }
+
+        for c in line.chars().rev() {
+            if let Some(x) = c.to_digit(10) {
+                res += x;
+                break;
+            }
+        }
+    }
+    dbg!(res);
 }
