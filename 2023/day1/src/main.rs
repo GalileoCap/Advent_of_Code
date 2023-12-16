349443cdd03c11f5e7a03602e293039be2215988
GalileoCap <galileocapp@gmail.com>
2023-12-16 10:59:35 -0300

    2023D2 OK: Part Two using Aho-Corasik crate

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -1,16 +1,31 @@
 use std::{env, fs, iter::Iterator};
+use lazy_static::lazy_static;
+use aho_corasick::{AhoCorasick, PatternID};
 
 fn match_first_digit_part1(line : String) -> u32 {
     line.chars().find_map(|c| c.to_digit(10)).unwrap()
 }
 
-fn match_first_digit_part2(line : String) -> u32 {
-    todo!()
+fn match_first_digit_part2(line : String, rev : bool) -> u32 {
+    lazy_static!{
+        static ref AC : AhoCorasick = AhoCorasick::new(
+            &["1", "one", "2", "two", "3", "three", "4", "four", "5", "five", "6", "six", "7", "seven", "8", "eight", "9", "nine"]
+        ).unwrap();
+        static ref AC_rev : AhoCorasick = AhoCorasick::new(
+            &["1", "eno", "2", "owt", "3", "eerht", "4", "ruof", "5", "evif", "6", "xis", "7", "neves", "8", "thgie", "9", "enin"]
+        ).unwrap();
+    }
+
+    if rev {
+        AC_rev.find(&line).unwrap().pattern().as_u32() / 2 + 1
+    } else {
+        AC.find(&line).unwrap().pattern().as_u32() / 2 + 1
+    }
 }
 
-fn match_first_digit(line : String, part_two : bool) -> u32 {
+fn match_first_digit(line : String, part_two : bool, rev : bool) -> u32 {
     if part_two {
-        match_first_digit_part2(line)
+        match_first_digit_part2(line, rev)
     } else {
         match_first_digit_part1(line)
     }
@@ -24,8 +39,8 @@ fn main() {
     let res = fs::read_to_string(fpath).unwrap().lines()
         .fold(0, |res, line|
             res
-            + match_first_digit(line.chars().collect(), part_two) * 10 
-            + match_first_digit(line.chars().rev().collect(), part_two)
+            + match_first_digit(line.chars().collect(), part_two, false) * 10 
+            + match_first_digit(line.chars().rev().collect(), part_two, true)
         );
     dbg!(res);
 }
