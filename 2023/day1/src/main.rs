cdaac7584327be3d67b253b808abcdab5d2d092a
GalileoCap <galileocapp@gmail.com>
2023-12-16 11:23:07 -0300

    2023D1 CLEAN

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs, iter::Iterator};
 use lazy_static::lazy_static;
-use aho_corasick::{AhoCorasick, PatternID};
+use aho_corasick::AhoCorasick;
 
 fn match_first_digit_part1(line : String) -> u32 {
     line.chars().find_map(|c| c.to_digit(10)).unwrap()
@@ -11,13 +11,13 @@ fn match_first_digit_part2(line : String, rev : bool) -> u32 {
         static ref AC : AhoCorasick = AhoCorasick::new(
             &["1", "one", "2", "two", "3", "three", "4", "four", "5", "five", "6", "six", "7", "seven", "8", "eight", "9", "nine"]
         ).unwrap();
-        static ref AC_rev : AhoCorasick = AhoCorasick::new(
+        static ref AC_REV : AhoCorasick = AhoCorasick::new(
             &["1", "eno", "2", "owt", "3", "eerht", "4", "ruof", "5", "evif", "6", "xis", "7", "neves", "8", "thgie", "9", "enin"]
         ).unwrap();
     }
 
     if rev {
-        AC_rev.find(&line).unwrap().pattern().as_u32() / 2 + 1
+        AC_REV.find(&line).unwrap().pattern().as_u32() / 2 + 1
     } else {
         AC.find(&line).unwrap().pattern().as_u32() / 2 + 1
     }
@@ -31,13 +31,21 @@ fn match_first_digit(line : String, part_two : bool, rev : bool) -> u32 {
     }
 }
 
-fn main() {
-    let lines_1 = &fs::read_to_string("inout/1.example.in").unwrap();
-    let lines_2 = &fs::read_to_string("inout/2.example.in").unwrap();
-    let lines = &fs::read_to_string("inout/input").unwrap();
+fn solve(file : &String, part_two : bool) -> u32 {
+    file.lines().fold(0, |res, line|
+        res
+        + match_first_digit(line.chars().collect(), part_two, false) * 10 
+        + match_first_digit(line.chars().rev().collect(), part_two, true)
+    )
+}
 
-    for (i, (lines, expected)) in
-        [lines_1, lines, lines_2, lines].iter()
+fn main() {
+    let file_1 = &fs::read_to_string("inout/1.example.in").unwrap();
+    let file_2 = &fs::read_to_string("inout/2.example.in").unwrap();
+    let file = &fs::read_to_string("inout/input").unwrap();
+
+    for (i, (file, expected)) in
+        [file_1, file, file_2, file].iter()
             .zip([142, 54390, 281, 54277])
             .enumerate()
         {
@@ -45,14 +53,9 @@ fn main() {
         let part_two = i >= 2;
 
         let now = Instant::now();
-        let res = lines.lines().fold(0, |res, line|
-                res
-                + match_first_digit(line.chars().collect(), part_two, false) * 10 
-                + match_first_digit(line.chars().rev().collect(), part_two, true)
-            );
+        let res = solve(file, part_two);
         let elapsed = now.elapsed();
         assert_eq!(expected, res);
         println!("{i}: {:?} {} ", elapsed, res);
     }
-    println!();
 }
