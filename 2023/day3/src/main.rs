43863a428e769938377f8b47c873a38fc42dd8a3
GalileoCap <galileocapp@gmail.com>
2023-12-17 15:01:30 -0300

    2023D3 INIT

---
+++ 2023/day3/src/main.rs
@@ -0,0 +1,39 @@
+type RES = u32;
+
+fn solve_part_one(file : &String) -> RES {
+    todo!()
+}
+
+fn solve_part_two(file : &String) -> RES {
+    todo!()
+}
+
+fn solve(file : &String, part_two : bool) -> RES {
+    if part_two {
+        solve_part_two(file)
+    } else {
+        solve_part_one(file)
+    }
+}
+
+fn main() {
+    use std::fs::read_to_string;
+    let file_1 = &read_to_string("inout/1.example.in").unwrap();
+    let file_2 = &read_to_string("inout/2.example.in").unwrap();
+    let file = &read_to_string("inout/input").unwrap();
+
+    for (i, (file, expected)) in
+        [file_1, file, file_2, file].iter()
+            .zip([0, 0, 0, 0])
+            .enumerate()
+        {
+        use std::time::Instant;
+        let part_two = i >= 2;
+
+        let now = Instant::now();
+        let res = solve(file, part_two);
+        let elapsed = now.elapsed();
+        assert_eq!(expected, res);
+        println!("{i}: {:?} {} ", elapsed, res);
+    }
+}
